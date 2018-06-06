c926635110003df86e8f463b580d9e6b0f9832e6
Nils <38754158+niiiils@users.noreply.github.com>
2018-06-06 10:48:57 +0200

    1

---
+++ app/src/main/java/net/ictcampus/fahrnin/musicloader/MainActivity.java
@@ -0,0 +1,20 @@
+package net.ictcampus.fahrnin.musicloader;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

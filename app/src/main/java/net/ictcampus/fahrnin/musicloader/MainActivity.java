e2a974012597b4b9eae45b08e13efdcc95d9b699
Nils <38754158+niiiils@users.noreply.github.com>
2018-06-08 10:39:25 +0200

    1/3

--- app/src/main/java/net/ictcampus/fahrnin/musicloader/MainActivity.java
+++ app/src/main/java/net/ictcampus/fahrnin/musicloader/MainActivity.java
@@ -1,20 +1,49 @@
 package net.ictcampus.fahrnin.musicloader;
 
-import android.content.pm.ActivityInfo;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.widget.SearchView;
+
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SearchView sv;
+    private List<SearchResult> lr;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         super.onCreate(savedInstanceState);
+        intent = new Intent(this,ScrollingActivity.class);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         setContentView(R.layout.activity_main);
+
+
+        sv = (SearchView) findViewById(R.id.searchBar);
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                waduhec(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+    }
+    public void waduhec(String s) {
+        intent.putExtra("Search",s);
+        startActivity(intent);
     }
 }
